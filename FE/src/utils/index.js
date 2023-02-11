dea12dfef7af682106ef9c2f5111815fb457fcfc
frank-ops <thejasfranklin@gmail.com>
2023-02-11 23:16:26 +0530

    Update index.js

--- FE/src/utils/index.js
+++ FE/src/utils/index.js
@@ -9,6 +9,23 @@ export function getRandomPrompt(prompt){
      return randomPrompt;
 }
 
+const blobURL = URL.createObjectURL(blob);
+    // Create the `<a download>` element and append it invisibly.
+    const a = document.createElement('a');
+    a.href = blobURL;
+    a.download = suggestedName;
+    a.style.display = 'none';
+    document.body.append(a);
+    // Programmatically click the element.
+    a.click();
+    // Revoke the blob URL and remove the element.
+    setTimeout(() => {
+      URL.revokeObjectURL(blobURL);
+      a.remove();
+    }, 1000);
+  };
 export async function downloadImg(_id,photo){
-    FileSaver.saveAs(photo)
+    const blob = await fetch(photo).then((response) => response.blob());
+    console.log(blob)
+ saveFile(blob,"download")
 }
